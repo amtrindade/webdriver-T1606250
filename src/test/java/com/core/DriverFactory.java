0cf891c62232926e45ba10fcc0b37fde3be8f997
Antonio Trindade <atrindade@umov.me>
2025-05-06 19:54:04 -0300

    Criado DriverFacotry

---
+++ src/test/java/com/core/DriverFactory.java
@@ -0,0 +1,34 @@
+package com.core;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+	
+	private static WebDriver driver = null;
+	
+	public static WebDriver getDriver() {
+		
+		if (driver == null) {
+			driver = createDriver();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		}
+				
+		return driver;
+	}
+	
+	private static WebDriver createDriver() {
+		
+		System.setProperty("webdriver.chrome.driver", "/Users/umov.me/Dev/drivers/chromedriver");
+		driver = new ChromeDriver();
+		
+		return driver;
+	}
+	
+	public static void killDriver() {
+		driver.quit();
+	}
+
+}
--- src/test/java/com/test/CalculadoraTest.java
+++ src/test/java/com/test/CalculadoraTest.java
@@ -1,55 +1,44 @@
 package com.test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.core.DriverFactory.getDriver;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.Duration;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class CalculadoraTest {
+import com.core.BaseTest;
+
+public class CalculadoraTest extends BaseTest{
 	
-	private WebDriver driver;
 	private WebDriverWait wait;
 
 	@BeforeEach
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver", "/Users/umov.me/Dev/drivers/chromedriver");
-		driver = new ChromeDriver();
 		
-		//espera implicita
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
-		driver.get("https://antoniotrindade.com.br/treinoautomacao/desafiosoma.html");
+		getDriver().get("https://antoniotrindade.com.br/treinoautomacao/desafiosoma.html");
 		//Cria o wait
-		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
 	
 	}
-
-	@AfterEach
-	public void tearDown() throws Exception {
-		driver.quit();
-	}
-	
+		
 	@Test
 	public void testSomaDoisNumeros() throws InterruptedException {
-		WebElement tfNumber1 = driver.findElement(By.id("number1"));
+		WebElement tfNumber1 = getDriver().findElement(By.id("number1"));
 		tfNumber1.sendKeys("78");
 		
-		WebElement tfNumber2 = driver.findElement(By.id("number2"));
+		WebElement tfNumber2 = getDriver().findElement(By.id("number2"));
 		tfNumber2.sendKeys("10");
 		
-		WebElement btnSomar = driver.findElement(By.id("somar"));
+		WebElement btnSomar = getDriver().findElement(By.id("somar"));
 		btnSomar.click();
 		
-		WebElement tfTotal = driver.findElement(By.id("total"));
+		WebElement tfTotal = getDriver().findElement(By.id("total"));
 		
 		//expera explícita		
 		wait.until(ExpectedConditions.textToBePresentInElementValue(tfTotal, "88"));
