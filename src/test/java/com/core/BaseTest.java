129098c6d08577e46e52bc37e05e3998d55b0cba
Antonio Trindade <atrindade@umov.me>
2025-05-08 19:57:23 -0300

    Edit Screenshot como evidencia

--- src/test/java/com/core/BaseTest.java
+++ src/test/java/com/core/BaseTest.java
@@ -1,11 +1,28 @@
 package com.core;
 
+import static com.core.DriverFactory.*;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.TestInfo;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import java.io.File;
+import java.time.LocalDateTime;
+
 
 public abstract class BaseTest {
 	
+		
 	@AfterEach
-	public void tearDown() throws Exception {
-		DriverFactory.killDriver();
+	public void tearDown(TestInfo testInfo) throws Exception {
+		
+		String nameTest = testInfo.getTestMethod().map(method -> method.getName()).orElse("test-desconhecido");
+		
+		//Captura a tela do browser que está aberta
+		File screen = ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(screen, new File("target"+ File.separator + nameTest+LocalDateTime.now()+".jpg"));		
+		
+		killDriver();
 	}
 }
