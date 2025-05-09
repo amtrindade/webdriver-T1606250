1fd89983c402a1bff404a4e1543fda706f15c922
Antonio Trindade <atrindade@umov.me>
2025-05-08 21:55:12 -0300

    Add AddCart

--- src/test/java/com/page/MainBSPage.java
+++ src/test/java/com/page/MainBSPage.java
@@ -11,5 +11,24 @@ public class MainBSPage {
 		WebElement divAvailable = getDriver().findElement(By.id("available"));
 		return divAvailable.isDisplayed();		
 	}
+	
+	public MainBSPage sendCoreJava(String quantity) {
+		WebElement tfCoreJava = getDriver().findElement(By.name("q"));
+		tfCoreJava.sendKeys(quantity);
+		
+		return this;
+	}
+
+	public MainBSPage clickAdd() {
+		WebElement btnAdd = getDriver().findElement(By.xpath("//input[@value='Add']"));
+		btnAdd.click();
+		
+		return this;		
+	}
+	
+	public String getValueTotal() {
+		WebElement tfTotal = getDriver().findElement(By.id("total"));
+		return tfTotal.getDomProperty("value");		
+	}
 
 }
